bfd68629d510db18eef9e56e27ff1bfdd4e2fc51
Flora Petersen <floramarina@comcast.net>
2020-06-06 01:58:40 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def my_all?(collection)
   i = 0
   block_return_values = []
@@ -7,9 +5,7 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i = i + 1
   end
-end
-
-if block_return_values.include?(false)
+  if block_return_values.include?(false)
     false
   else
     true
