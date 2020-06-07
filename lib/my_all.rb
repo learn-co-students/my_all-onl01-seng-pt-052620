a18791e45bfbf71144f77f830cae4dc01ce43591
Jack Goode <jack.w.goode@icloud.com>
2020-06-07 19:17:30 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,23 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  block_return_values = []
+  while i < collection.length
+  block_return_values << yield(collection[i])
+  i += 1   
+ end
+ 
+ if block_return_values.include?(false)
+   false
+ else
+   true 
+ end
 end
+
+collection = [1,2,3]
+
+my_all?(collection) do
+collection.all? {|i| i < 2}
+end
+  
